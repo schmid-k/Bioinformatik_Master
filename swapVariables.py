224d3d586a4327a1597038d388db891e16c835e0
kilis <kilian.schmid@stud-mail.uni-wuerzburg.de>
2024-02-02 12:40:00 +0100

    Erstellung swapVariables

---
+++ swapVariables.py
@@ -0,0 +1,13 @@
+eins = "zwei"
+zwei = "eins"
+
+# Tausche den Wert der Variablen
+
+# cache = eins
+# eins = zwei
+# zwei = cache
+
+eins, zwei = zwei, eins
+
+print("Eins ist ", eins)
+print("Zwei ist ", zwei)
