2cb27a5843aca586dd504b3a990262d664b3bdf0
kilis <kilian.schmid@stud-mail.uni-wuerzburg.de>
2024-02-02 12:38:29 +0100

    Erstellung erstesProgramm

---
+++ erstesProgramm.py
@@ -0,0 +1,15 @@
+print("Hallo Welt")
+print(4 + 5)
+
+willi = 5   # Int ->
+print(willi)
+
+name = "Maja"
+print(name)
+
+single_quoted = 'This is a string with "double quotes" inside'
+double_quoted = "This is a string with 'single quotes' inside"
+print(double_quoted)
+
+myBoolean = True
+print(myBoolean)
